88b51ee24004bc3acf6c46cd0c5f6e74a288b0fd
Josh Higgins <jshhggns@gmail.com>
2015-05-05 12:54:38 -0400

    Palindrome test with iteration

---
+++ palindrome.py
@@ -0,0 +1,36 @@
+import re
+
+def make_text(text):
+    """Takes text and removes punctuation and spaces"""
+    return (re.sub(r'[^A-Za-z]',"", text)).lower()
+
+
+
+def palindrome(text):
+    """Takes text, removes punctuation and spaces, and
+    returns 'is a palindrome' or 'is not a palindrome'"""
+    length = len(text)
+    test_a = 0
+    test_b = length - 1
+    counter = length // 2
+
+    if length == 1:
+        return "is a palindrome"
+
+    while counter > 0:
+        if text[test_a] != text[test_b]:
+            return "is not a palindrome"
+        else:
+            counter -= 1
+    return "is a palindrome"
+
+#    length = len(text)
+#    if length == 0:
+#        return "is a palindrome"
+#    else:
+#        return "is not a palindrome"
+
+test_case = input("What is your text? ")
+corrected_test = make_text(test_case)
+
+print(palindrome(corrected_test))
