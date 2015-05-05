f2af87341f8cd584f251445d1e4dbd552de70dc8
sovello <sovellohpmgani@gmail.com>
2015-05-05 15:17:41 -0400

    the palindrome tester now uses a function

--- palindrome.py
+++ palindrome.py
@@ -1,16 +1,25 @@
 from re import *
+
+def isapalindrome( text ):
+    '''returns True when text is a palindrome'''
+    # user_text_lower
+    text_lower = text.lower()
+
+    # sub(pattern, repl, string, count=0, flags=0)
+    cleaned_text = sub(r'[^A-Za-z]', "", text_lower)
+
+    # determine if the text is palindrome or not
+    if cleaned_text == cleaned_text[::-1]:
+        return True
+    else:
+        return False
+    # let the user know if it is a palindrome or not
+
 # ask the user to input some text
 user_text = input("Enter some text: ")
 
-# user_text_lower
-user_text_lower = user_text.lower()
 
-# sub(pattern, repl, string, count=0, flags=0)
-cleaned_text = sub(r'[^A-Za-z]', "", user_text_lower)
-
-# determine if the text is palindrome or not
-if cleaned_text == cleaned_text[::-1]:
-    print("{} is a palindrome".format(cleaned_text))
+if isapalindrome(user_text):
+    print("{} is a palindrome".format(user_text))
 else:
-    print("{} is not a palindrome".format(cleaned_text))
-# let the user know if it is a palindrome or not
+    print("{} is not a palindrome".format(user_text))
