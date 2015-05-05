d160e37b38db9ede7dc17404c06d6ee9ffc63e9d
sovello <sovellohpmgani@gmail.com>
2015-05-05 18:13:06 -0400

    separated the recursive function and the normal mode into two files

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,6 @@
 from re import *
 
-def isapalindrome( ):
+def isAPalindrome( ):
     '''returns True when text is a palindrome'''
     # user_text_lower
     text = input("Write some text: ")
@@ -19,28 +19,4 @@ def isapalindrome( ):
 # ask the user to input some text
 #user_text = input("Enter some text: ")
 
-
-
-def reverseChar(word = '' ):
-
-    reversed_word = ''
-    for letter in word:
-        if len(word) == 1:
-            reversed_word = reversed_word + word
-        else:
-            reversed_word =  reversed_word+word[-1]
-            word = word[:len(word)-1]
-            reverseChar(word)
-
-    return reversed_word
-
-
-word = input("Write some text: ")
-
-# determine if the text is palindrome or not
-reversed_word = sub(r'[^A-Za-z]', "",reverseChar(word))
-original_word = sub(r'[^A-Za-z]', "", word)
-if original_word.lower() == reversed_word.lower():
-    print("is a palindrome")
-else:
-    print("is not a palindrome")
+isAPalindrome()
