4c48f3736a79c8518f6445a27d22ddcf8be67fc8
Jeremy Gresham <greshjs@gmail.com>
2015-05-05 13:35:49 -0400

    recursive palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,19 +2,26 @@ import re
 string = input("Enter your Text: ")
 string = re.sub(r'[^A-Za-z]',"",string)
 string = string.lower()
-
-
-def ite(inp):
+print(string)
+def rec(inp):
   inp = inp[:]
-  while len(inp)>1:
+
+  if len(inp)>2:
       if inp[0]==inp[-1]:
-          inp = inp[1:-1]
-          continue
+          return rec(inp[1:-1])
       else:
           return False
-  return True
 
-if ite(string):
+  else:
+      if(len(inp)==1):
+          return True
+      else:
+          if inp[0]==inp[-1]:
+              return True
+          else:
+              return False
+
+if rec(string):
     print("\"{}\" is a palindrome!".format(string))
 else:
     print("\"{}\" is not a palindrome!".format(string))
